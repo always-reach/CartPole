2465f393777c1898371e3e25495debed5efb42ec
always-reach <67514209+always-reach@users.noreply.github.com>
2022-06-15 23:55:05 +0900

    fix q_table

--- main.py
+++ main.py
@@ -11,32 +11,31 @@ def run():
     gym_env: GymEnv = GymEnv()
     keras_model: KerasModel = KerasModel()
     num_episodes: int = 3000
-    max_score = 200.0
     score = []
     for episode in range(num_episodes):
         print("episode", episode)
         state = gym_env.env.reset()
-        gym_env.env.render()
         x, q, a, r = [], [], [], []
         game = False
         while not game:
-            q_value = keras_model.q_values(state)
-            action = np.argmax(q_value)
+            q_value: list[float] = keras_model.q_values(state)
+            action: int = np.argmax(q_value)
+            state, reward, game, info = gym_env.env.step(action)
+
             x.append(state)
             a.append(action)
             q.append(q_value)
 
-            state, reward, game, info = gym_env.env.step(action)
+            gym_env.env.render()
             r.append(reward)
 
-        alpha = 0.1
         batch_size = len(x)
+        # 1エピソードをバッチサイズとする
+        q_tabel = keras_model.update_q(q, x, r, a, batch_size)
+
         gross_reward = np.sum(r)
 
-        for index in range(batch_size):
-            past_action = a[index]
-            q[index][past_action] = (1 - alpha) * q[index][past_action] + alpha * (gross_reward / max_score)
-        keras_model.learn(x, q, batch_size)
+        keras_model.model.fit(np.asarray(x), np.asarray(q_tabel), verbose=0, batch_size=batch_size)
         score.append(gross_reward)
 
     logger.info("Average Reward: %.3f" % np.mean(score))
